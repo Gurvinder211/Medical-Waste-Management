16c52fd7caa6d8072f84fe8fe09622bf4db4e8b9
Gurvinder Singh Mudhar <gsm21102004@gmail.com>
2025-04-03 22:09:18 +0530

    bug fixing

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,18 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend")));
 app.use(cors({ origin: "*", credentials: true }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+  }
+  next();
+});
+
+
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/waste", wasteRoutes);
