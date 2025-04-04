7a634debe4fdf50c8240acc3dc3e9ab063515a5c
Gurvinder Singh Mudhar <gsm21102004@gmail.com>
2025-04-04 21:24:47 +0530

    bug fixing

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,14 +4,14 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["provider", "taker"], required: true }, // Role-based Access
+  role: { type: String, enum: ["provider", "collector"], required: true }, // Role-based Access
 
   // Fields specific to "providers" (Hospitals/Clinics)
   address: { type: String, required: function () { return this.role === "provider"; } },
 
   // Fields specific to "takers" (Waste Collectors)
-  companyName: { type: String, required: function () { return this.role === "taker"; } },
-  licenseNumber: { type: String, required: function () { return this.role === "taker"; } },
+  companyName: { type: String, required: function () { return this.role === "collector"; } },
+  licenseNumber: { type: String, required: function () { return this.role === "collector"; } },
 
   createdAt: { type: Date, default: Date.now },
 });
